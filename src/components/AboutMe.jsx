6a6c5c8026fa52e2a1d4eed193eb3d32a036d8fa
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-01 16:06:07 -0700

    update src of images to work on production

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,7 +4,7 @@ function AboutMe() {
             <h2 className="font-bold text-xl md:text-2xl xl:text-4xl m-2 text-center">About Me</h2>
             <div className="flex flex-col justify-center items-center">
                 <figure className="flex justify-center items-center">
-                    <img className="rounded-md m-3 max-w-[30vw]" src="./src/assets/Me-at-the-beach.jpg" alt="Me at the beach" />
+                    <img className="rounded-md m-3 max-w-[30vw]" src="./Me-at-the-beach.jpg" alt="Me at the beach" />
                     <figcaption className="text-center max-w-[90%] lg:max-w-[50%]">
                         I currently live in Oregon with my girlfriend and our two cats.
                         My web development journey started when I was 14 in high school and I took a class on HTML, CSS, and Javascript.
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,7 +9,7 @@ function Projects() {
             frontend: ["Vue 3", "Vue Router", "Tailwind CSS via CLI", "Google Maps API"],
             backend: [".NET", "ASP.NET", "Entity Framework Core", "Azure Services", "Azure Blob Storages", "SQL Server", "Polly", "Dapper"],
             other: [],
-            image: "./src/assets/boise-tuxedo-shop.png",
+            image: "./boise-tuxedo-shop.png",
             deployed: "https://www.boisetuxedoshop.com",
             github: ""
         },
@@ -19,7 +19,7 @@ function Projects() {
             frontend: ["JQuery", "Tailwind CSS via CDN"],
             backend: ["Pokeapi"],
             other: [],
-            image: "./src/assets/pokepedia.png",
+            image: "./pokepedia.png",
             deployed: "https://shhutc18.github.io/Poke-Pedia/",
             github: "https://github.com/shhutc18/Poke-Pedia"
         },
@@ -29,7 +29,7 @@ function Projects() {
             frontend: ["Handlebars.js", "Tailwind CSS via CDN"],
             backend: ["Node.js", "Express", "Sequelize", "Passport.js", "Mysql", "Express Sessions", "Express Handlebars", "Dotenv", "Crypto"],
             other: [],
-            image: "./src/assets/daily-planner.png",
+            image: "./daily-planner.png",
             deployed: "https://rocky-mountain-31745-6823d73c66a2.herokuapp.com/login",
             github: "https://github.com/shhutc18/Daily-Planner/"
         },
@@ -43,7 +43,7 @@ function Projects() {
             frontend: [],
             backend: ["IndexedDB", "Workbox Service Worker", "Express"],
             other: ["Code Mirror", "Webpack"],
-            image: "./src/assets/text-editor.png",
+            image: "./text-editor.png",
             deployed: "https://pwa-text-editor-x7hj.onrender.com/",
             github: "https://github.com/ChaseLee21/PWA-Text-Editor"
         },
@@ -60,7 +60,7 @@ function Projects() {
             frontend: [],
             backend: ["Node.js", "Mysql", "Express", "sequelize", "Dotenv"],
             other: [],
-            image: "./src/assets/ecommerce.png",
+            image: "./ecommerce.png",
             deployed: "https://drive.google.com/file/d/1PLICnX2UrKDw8F4uEQRnvIrsyHUJIJWK/view?pli=1",
             github: "https://github.com/ChaseLee21/Ecommerce-Backend"
         },
@@ -77,7 +77,7 @@ function Projects() {
             frontend: [],
             backend: ["Node.js", "Mongoose", "Express", "MongoDB"],
             other: [],
-            image: "./src/assets/social-network.png",
+            image: "./social-network.png",
             deployed: "https://drive.google.com/file/d/1P5P0UGtThCpoft0UOY2Y5RlqbxU0FLaX/view",
             github: "https://github.com/ChaseLee21/Social-Network-Api"
         },
