e1fafefb00c0efbddefe1b89bc5e7e36daa9e7e0
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-01 16:02:10 -0700

    updated h1 tag to link to about me page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <header className="bg-blue-950 bg-opacity-85 flex flex-row flex-nowrap items-center justify-between">
-        <h1 className="text-white font-bold text-xl md:text-2xl xl:text-4xl m-2">Chase Seeberger</h1>
+        <h1 className="text-white font-bold text-xl md:text-2xl xl:text-4xl m-2"><Link to="/">Chase Seeberger</Link></h1>
         <nav className="text-white font-bold text-base md:text-lg xl:text-2xl">
             <ul className="flex flex-row justify-evenly m-2">
                 <li className="px-2"><Link to="/">About Me</Link></li>
