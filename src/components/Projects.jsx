ded604a6d07e47f64b7fd0985f236e0d05d79dee
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-01 12:35:48 -0700

    styling changes to project component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,77 +3,89 @@ function Projects() {
         <main>
             <section className="bg-white rounded m-2 p-2 lg:m-4 xl:m-8">
                 <h2 className="font-bold text-xl md:text-2xl xl:text-4xl m-2 text-center">Projects</h2>
-                <article className="Project">
-                    <h3>Boise Tuxedo Shop</h3>
-                    <a href="https://www.boisetuxedoshop.com" target="_blank">Deployed Project</a>
-                    <p>Github Repository: private</p>
+                <article className="flex flex-col">
+                    <h3 className="text-center text-lg lg:text-2xl">Boise Tuxedo Shop</h3>
+                    <a className="text-center underline text-blue-600" href="https://www.boisetuxedoshop.com" target="_blank">Deployed Project</a>
+                    <p className="text-center">Github Repository: private</p>
                     <p>Boisetuxedoshop.com is a public facing website I made for my families tuxedo and suit rental business. 
                     Users can view information such as store hours, product images and descriptions, pricing for services, information about events such as prom, and more.
                     Some of the technologies used to build the application are listed below.</p>
-                    <h4>Frontend:</h4>
-                    <ul>
-                        <li>Vue 3</li>
-                        <li>Vue Router</li>
-                        <li>Tailwind CSS via CLI</li>
-                        <li>Google Maps API</li>
-                    </ul>
-                    <h4>Backend:</h4>
-                    <ul>
-                        <li>.NET</li>
-                        <li>ASP.NET</li>
-                        <li>Entity Framework Core</li>
-                        <li>Azure Services</li>
-                        <li>Azure Blob Storages</li>
-                        <li>SQL Server</li>
-                        <li>Polly</li>
-                        <li>Dapper</li>
-                    </ul>
+                    <div className="border-black rounded border-2 p-2 m-1">
+                        <h4 className="text-lg">Frontend</h4>
+                        <ul className="list-disc list-inside">
+                            <li>Vue 3</li>
+                            <li>Vue Router</li>
+                            <li>Tailwind CSS via CLI</li>
+                            <li>Google Maps API</li>
+                        </ul>
+                    </div>
+                    <div className="border-black rounded border-2 p-2 m-1">
+                        <h4>Backend:</h4>
+                        <ul className="list-disc list-inside">
+                            <li>.NET</li>
+                            <li>ASP.NET</li>
+                            <li>Entity Framework Core</li>
+                            <li>Azure Services</li>
+                            <li>Azure Blob Storages</li>
+                            <li>SQL Server</li>
+                            <li>Polly</li>
+                            <li>Dapper</li>
+                        </ul>
+                    </div>
                 </article>
                 <article className="Project">
-                    <h3>Poke-Pedia</h3>
-                    <a href="https://shhutc18.github.io/Poke-Pedia/">Depolyed Project</a>
+                    <h3 className="text-center text-lg lg:text-2xl">Poke-Pedia</h3>
+                    <a className="text-center" href="https://shhutc18.github.io/Poke-Pedia/">Depolyed Project</a>
                     <a href="https://github.com/shhutc18/Poke-Pedia" target="_blank">Github Repository</a>
                     <p>Poke-Pedia was the first group project I worked on with classmates in my coding bootcamp. 
                     It uses the <a href="https://pokeapi.co/" target="_blank">Pokeapi</a> to fetch all 1118 Pokemon.
                     We then used this data to create an encyclopedia that allows users to search & view Pokemon.
                     This was a really fun project for me to work on and it encapsulates a part of my coding journey that I will remember for a long time. </p>
-                    <h4>Frontend:</h4>
-                    <ul>
-                        <li>JQuery</li>
-                        <li>Tailwind CSS via CDN</li>
-                        <li>Pokeapi</li>
-                    </ul>
-                    <h4>Backend:</h4>
-                    <p>This project was early on in the coding bootcamp and there is no backend.</p>
+                    <div className="border-black rounded border-2 p-2 m-1">
+                        <h4>Frontend:</h4>
+                        <ul className="list-disc list-inside">
+                            <li>JQuery</li>
+                            <li>Tailwind CSS via CDN</li>
+                            <li>Pokeapi</li>
+                        </ul>
+                    </div>
+                    <div className="border-black rounded border-2 p-2 m-1">
+                        <h4>Backend:</h4>
+                        <p>This project was early on in the coding bootcamp and there is no backend.</p>
+                    </div>
                 </article>
                 <article className="Project">
-                    <h3>Daily Planner</h3>
+                    <h3 className="text-center text-lg lg:text-2xl">Daily Planner</h3>
                     <a href="https://rocky-mountain-31745-6823d73c66a2.herokuapp.com/">Depolyed Project</a>
                     <a href="https://github.com/shhutc18/Daily-Planner" target="_blank">Github Repository</a>
                     <p>Daily Planner was the second group project I worked on in my coding bootcamp. 
                     We had two main goals for this project: first was to create a daily planner using the model, view, controller (MVC) design pattern, and second was to implement a user authentication system.
                     We used a handful of new technologies to achieve these goals and I learned a lot about how to structure a project in a way that is scalable and maintainable.
                     Below are some of the technologies used to build the application.</p>
-                    <h4>Frontend:</h4>
-                    <ul>
-                        <li>Handlebars.js</li>
-                        <li>Tailwind CSS via CDN</li>
-                    </ul>
-                    <h4>Backend:</h4>
-                    <ul>
-                        <li>Node.js</li>
-                        <li>Sequelize</li>
-                        <li>Passport.js</li>
-                        <li>Mysql</li>
-                        <li>Express</li>
-                        <li>Express Sessions</li>
-                        <li>Express Handlebars</li>
-                        <li>Dotenv</li>
-                        <li>Crypto</li>
-                    </ul>
+                    <div className="border-black rounded border-2 p-2 m-1">
+                        <h4>Frontend:</h4>
+                        <ul className="list-disc list-inside">
+                            <li>Handlebars.js</li>
+                            <li>Tailwind CSS via CDN</li>
+                        </ul>
+                    </div>
+                    <div className="border-black rounded border-2 p-2 m-1">
+                        <h4>Backend:</h4>
+                        <ul className="list-disc list-inside">
+                            <li>Node.js</li>
+                            <li>Sequelize</li>
+                            <li>Passport.js</li>
+                            <li>Mysql</li>
+                            <li>Express</li>
+                            <li>Express Sessions</li>
+                            <li>Express Handlebars</li>
+                            <li>Dotenv</li>
+                            <li>Crypto</li>
+                        </ul>
+                    </div>
                 </article>
                 <article className="Project">
-                    <h3>Text Editor - Progressive Web Application</h3>
+                    <h3 className="text-center text-lg lg:text-2xl">Text Editor - Progressive Web Application</h3>
                     <a href="https://pwa-text-editor-x7hj.onrender.com/">Depolyed Project</a>
                     <a href="https://github.com/ChaseLee21/PWA-Text-Editor" target="_blank">Github Repository</a>
                     <p>This project was apart of my coding bootcamp at University of Oregon for web development. 
@@ -84,19 +96,23 @@ function Projects() {
                     2 - Implement an indexedDB database that would store the users code between sessions.
                     3 - Implement a service worker that would cache images, scripts, and css.
                     </p>
-                    <h4>Backend:</h4>
-                    <ul>
-                        <li>IndexedDB</li>
-                        <li>Workbox Service Worker</li>
-                        <li>express</li>
-                    </ul>
-                    <h4>Other Packages:</h4>
-                    <ul>
-                        <li>Webpack</li>
-                    </ul>
+                    <div className="border-black rounded border-2 p-2 m-1">
+                        <h4>Backend:</h4>
+                        <ul className="list-disc list-inside">
+                            <li>IndexedDB</li>
+                            <li>Workbox Service Worker</li>
+                            <li>express</li>
+                        </ul>
+                    </div>
+                    <div className="border-black rounded border-2 p-2 m-1">
+                        <h4>Other Packages:</h4>
+                        <ul className="list-disc list-inside">
+                            <li>Webpack</li>
+                        </ul>
+                    </div>
                 </article>
                 <article className="Project">
-                    <h3>Ecommerce Backend</h3>
+                    <h3 className="text-center text-lg lg:text-2xl">Ecommerce Backend</h3>
                     <a href="https://github.com/ChaseLee21/Ecommerce-Backend" target="_blank">Github Repository</a>
                     <a href="https://drive.google.com/file/d/1PLICnX2UrKDw8F4uEQRnvIrsyHUJIJWK/view?pli=1" target="_blank">Demonstration Video</a>
                     <p>This project was apart of my coding bootcamp at University of Oregon for web development.
@@ -108,17 +124,19 @@ function Projects() {
                     4 - A demonstration video that would show the backend in action.
                     I really enjoyed this application because I have worked in ecommerce before and I was able to see how things function under the hood.
                     </p>
-                    <h4>Backend:</h4>
-                    <ul>
-                        <li>Node</li>
-                        <li>express</li>
-                        <li>mysql</li>
-                        <li>sequelize</li>
-                        <li>dotenv</li>
-                    </ul>
+                    <div className="border-black rounded border-2 p-2 m-1">
+                        <h4>Backend:</h4>
+                        <ul className="list-disc list-inside">
+                            <li>Node</li>
+                            <li>express</li>
+                            <li>mysql</li>
+                            <li>sequelize</li>
+                            <li>dotenv</li>
+                        </ul>
+                    </div>
                 </article>
                 <article className="Project">
-                    <h3>Social Network Backend</h3>
+                    <h3 className="text-center text-lg lg:text-2xl">Social Network Backend</h3>
                     <a href="https://github.com/ChaseLee21/Social-Network-Api" target="_blank">Github Repository</a>
                     <a href="https://drive.google.com/file/d/1P5P0UGtThCpoft0UOY2Y5RlqbxU0FLaX/view" target="_blank">Demonstration Video</a>
                     <p>
@@ -131,12 +149,14 @@ function Projects() {
                     4 - A demonstration video that would show the backend in action.
                     This was the first project where I used mongoose to interact with a mongoDB database.
                     </p>
-                    <h4>Backend:</h4>
-                    <ul>
-                        <li>Node</li>
-                        <li>express</li>
-                        <li>mongoose</li>
-                    </ul>
+                    <div className="border-black rounded border-2 p-2 m-1">
+                        <h4>Backend:</h4>
+                        <ul className="list-disc list-inside">
+                            <li>Node</li>
+                            <li>express</li>
+                            <li>mongoose</li>
+                        </ul>
+                    </div>
                 </article>
             </section>
         </main>
