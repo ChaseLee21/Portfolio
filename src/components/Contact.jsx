a39af3e31c7dfa1e872420ba134c9aa328653859
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-01 15:14:40 -0700

    styling changes to footer and resume components

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,9 +3,9 @@ function Contact() {
         <section className="bg-white rounded m-2 p-2 lg:m-4 xl:m-8">
             <h2 className="text-xl md:text-2xl xl:text-4xl text-center font-bold mb-2">Contact</h2>
             <p className="text-center text-base lg:text-xl">
-                Feel free to reach out to me with any questions or inquiries by: filling out the form below, sending me an email, or via phone.
+                Feel free to reach out to me with any questions or inquiries by filling out the form below
             </p>
-            <form className="flex flex-col m-auto text-base max-w-[90%] lg:max-w-[30%] bg-blue-950  p-2">
+            <form className="flex flex-col my-2 rounded-md mx-auto text-base max-w-[90%] lg:max-w-[30%] bg-blue-950 p-2">
                 <label className="text-lg lg:text-xl text-white" htmlFor="name">Name:</label>
                 <input className="rounded-md p-1" type="text" id="name" name="name" required></input>
                 <label className="text-lg lg:text-xl text-white" htmlFor="email">Email:</label>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,12 @@ function Footer() {
     return (
         <footer className="sticky bg-white flex flex-col justify-center items-center rounded m-2 pt-2 lg:mt-4 xl:mt-8 mx-2 lg:mx-4 xl:mx-8">
             <section className="flex">
-                <a className="mx-2" href="https://github.com/ChaseLee21" target="_blank">Github</a>
-                <a className="mx-2" href="https://www.linkedin.com/in/chaseseeberger/" target="_blank">LinkedIn</a>
-                <p className="mx-2">(208) 995 - 5987</p>
-                <p className="mx-2">chase.seeberger@gmail.com</p>
+                <a className="mx-2 text-base md:text-lg lg:text-xl underline hover:text-blue-950 text-blue-600" href="https://github.com/ChaseLee21" target="_blank">Github</a>
+                <a className="mx-2 text-base md:text-lg lg:text-xl underline hover:text-blue-950 text-blue-600" href="https://www.linkedin.com/in/chaseseeberger/" target="_blank">LinkedIn</a>
+                <p className="mx-2 text-base md:text-lg lg:text-xl text-blue-600">2089955987</p>
+                <p className="mx-2 text-base md:text-lg lg:text-xl text-blue-600">chase.seeberger@gmail.com</p>
             </section>
-            <section>
+            <section className="my-2">
                 <p className="text-sm">Created by Chase Seeberger</p>
             </section>
         </footer>
