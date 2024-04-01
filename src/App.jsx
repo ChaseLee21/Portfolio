554454d02f165e090eb5d71b96e980f061c31e93
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-01 11:22:15 -0700

    implemented react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import AboutMe from './components/AboutMe.jsx';
@@ -9,20 +7,19 @@ import Projects from './components/Projects.jsx';
 import Contact from './components/Contact.jsx';
 import './App.css'
 
-
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
-    <>
+    <Router>
       <Header />
-      <AboutMe />
-      <Projects />
-      <Resume />
-      <Contact />
+      <Switch>
+        <Route path="/aboutme" component={AboutMe} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/resume" component={Resume} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
       <Footer />
-    </>
+    </Router>
   )
 }
 
-export default App
+export default App;
