f6158c3186379531efc3ddacd3f86f878c08f325
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-01 10:16:58 -0700

    finished the content for the projects components

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,6 @@
 function Projects() {
     return (
-        <section>
+        <main>
             <h2>Projects</h2>
             <section className="ProjectContainer">
                 <article className="Project">
@@ -96,15 +96,50 @@ function Projects() {
                     </ul>
                 </article>
                 <article className="Project">
-                    <h3>Project 5</h3>
-                    <p>Project Description</p>
+                    <h3>Ecommerce Backend</h3>
+                    <a href="https://github.com/ChaseLee21/Ecommerce-Backend" target="_blank">Github Repository</a>
+                    <a href="https://drive.google.com/file/d/1PLICnX2UrKDw8F4uEQRnvIrsyHUJIJWK/view?pli=1" target="_blank">Demonstration Video</a>
+                    <p>This project was apart of my coding bootcamp at University of Oregon for web development.
+                    My task was to create a backend that could be implement into an ecommerce website using node.js, express.js, mysql, and sequelize.
+                    The backend was to have the following features:
+                    1 - A database with tables for products, categories, tags, and a product tag junction table.
+                    2 - API routes for all CRUD operations for each table.
+                    3 - A seed file that would populate the database with data.
+                    4 - A demonstration video that would show the backend in action.
+                    I really enjoyed this application because I have worked in ecommerce before and I was able to see how things function under the hood.
+                    </p>
+                    <h4>Backend:</h4>
+                    <ul>
+                        <li>Node</li>
+                        <li>express</li>
+                        <li>mysql</li>
+                        <li>sequelize</li>
+                        <li>dotenv</li>
+                    </ul>
                 </article>
                 <article className="Project">
-                    <h3>Project 6</h3>
-                    <p>Project Description</p>
+                    <h3>Social Network Backend</h3>
+                    <a href="https://github.com/ChaseLee21/Social-Network-Api" target="_blank">Github Repository</a>
+                    <a href="https://drive.google.com/file/d/1P5P0UGtThCpoft0UOY2Y5RlqbxU0FLaX/view" target="_blank">Demonstration Video</a>
+                    <p>
+                    This project was apart of my coding bootcamp at University of Oregon for web development.
+                    My task was to create a backend for a social network using node.js, express.js, and mongoose.
+                    The backend was to have the following features:
+                    1 - A database with tables for users, thoughts, and reactions.
+                    2 - API routes for all CRUD operations for each table.
+                    3 - A seed file that would populate the database with data.
+                    4 - A demonstration video that would show the backend in action.
+                    This was the first project where I used mongoose to interact with a mongoDB database.
+                    </p>
+                    <h4>Backend:</h4>
+                    <ul>
+                        <li>Node</li>
+                        <li>express</li>
+                        <li>mongoose</li>
+                    </ul>
                 </article>
             </section>
-        </section>
+        </main>
     );
   }
   
