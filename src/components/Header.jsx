b3e8605ab6bd64eab40cde613c2ab647e0ea3940
Tiny Penguin <chase.seeberger@gmail.com>
2024-06-13 16:51:55 -0700

    updated header to be mobile friendly

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <header className="bg-blue-950 bg-opacity-85 flex flex-row flex-nowrap items-center justify-between">
-        <h2 className="text-white font-bold text-lg md:text-xl m-2">Junior Web Developer</h2>
-        <h1 className="text-white font-bold text-xl md:text-2xl xl:text-4xl m-2"><Link to="/">Chase Seeberger</Link></h1>
-        <nav className="text-white font-bold text-base md:text-lg xl:text-2xl">
+        <div className='flex flex-col text-center' to="/">
+          <h1 className="text-white font-bold text-lg md:text-2xl xl:text-4xl mx-2">Chase Seeberger</h1>
+          <h2 className="text-white font-bold md:text-xl mx-2">Junior Web Developer</h2>
+        </div>
+        <nav className="text-white font-bold text-center text-base md:text-lg xl:text-2xl">
             <ul className="flex flex-row justify-evenly m-2">
                 <li className="px-2"><Link to="/">About Me</Link></li>
                 <li className="px-2"><Link to="/projects">Projects</Link></li>
