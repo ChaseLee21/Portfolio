a2eb724b0a539d9ec5b434e6a12762bc5c2d6203
Tiny Penguin <chase.seeberger@gmail.com>
2024-06-13 16:46:09 -0700

    updated job title

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -6,7 +6,7 @@ function Resume() {
                 My resume via google docs is viewable <a className="underline text-blue-600 hover:text-blue-950" href="https://docs.google.com/document/d/1LSksgt3VEu0PH06z9tfKMkIzejsrOjIq2umAH9SYB5w/edit#heading=h.f2c7u4yny38a" target="_blank" rel="noopener noreferrer">here</a>.
             </p>
             <p className="text-center text-base lg:text-xl m-auto max-w-[90%] lg:max-w-[50%] p-2 indent-5">
-                I am currently seeking a full-time position as a web developer. 
+                I am currently seeking a full-time position as a junior web developer. 
                 I am living in Salem, Oregon and may be willing to relocate for the right opportunity.
             </p>
         </section>
