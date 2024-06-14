5b6d889688fe64d3ac4a3519a66200dd85db72be
Tiny Penguin <chase.seeberger@gmail.com>
2024-06-13 17:00:56 -0700

    updated foot to fix a visual bug on mobile devices

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <footer className="sticky bg-white bg-opacity-85 flex flex-col justify-center items-center rounded m-2 pt-2 lg:mt-4 xl:mt-8 mx-2 lg:mx-4 xl:mx-8">
-            <section className="flex">
+            <section className="flex-col text-center">
                 <a className="mx-2 text-base md:text-lg lg:text-xl underline hover:text-blue-950 text-blue-600" href="https://github.com/ChaseLee21" target="_blank">Github</a>
                 <a className="mx-2 text-base md:text-lg lg:text-xl underline hover:text-blue-950 text-blue-600" href="https://www.linkedin.com/in/chaseseeberger/" target="_blank">LinkedIn</a>
                 <p className="mx-2 text-base md:text-lg lg:text-xl text-blue-600">2089955987</p>
